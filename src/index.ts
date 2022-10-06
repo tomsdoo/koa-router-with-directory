08597830ebb4ab8e2494312369268933c4be736f
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-07 05:41:21 +0900

    prettier is added (#21)
    * prettier is installed
    
    * npm script format:src is added
    
    * format:src
    
    * npm script format:test is added
    
    * format:test
    
    * tsignore

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,61 @@ import Router from "koa-router";
 import * as fs from "fs";
 import * as path from "path";
 
-export async function attachDirToRouter( router: Router, provided_path: string ) {
-  const files = await (async function readdirRecursively(dir: string, files: string[] = []) {
+export async function attachDirToRouter(router: Router, provided_path: string) {
+  const files = await (async function readdirRecursively(
+    dir: string,
+    files: string[] = []
+  ) {
     const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
-    const dirs : string[] = [];
-    for(const dirent of dirents) {
+    const dirs: string[] = [];
+    for (const dirent of dirents) {
       const mypath = `${dir}/${dirent.name}`;
-      if(dirent.isDirectory()){
+      if (dirent.isDirectory()) {
         dirs.push(mypath);
-      }else if(dirent.isFile()){
+      } else if (dirent.isFile()) {
         files.push(mypath);
       }
     }
-    for(const d of dirs) {
+    for (const d of dirs) {
       files = await readdirRecursively(d, files);
     }
     return files;
-  })
-  (provided_path);
+  })(provided_path);
 
   files
-  // .filter((file) => path.extname(file) === ".js")
-  .filter((file) => path.basename(file) === "index.js")
-  .forEach((file) => {
-    const f = path.relative(provided_path, file);
-    const tempm = require(file);
-    const mpath = `/${f.slice(0, f.lastIndexOf(path.sep)+1)}`.replace(/_/g, ":");
-    const basename = path.basename(f, path.extname(f));
-    ["get","post","put","delete"]
-    .map((method) => {
-      for(const tempmethod in tempm){
-        if(tempmethod.toUpperCase() === method.toUpperCase()){return { method: method, functionname:tempmethod };}
-      }
-      return {method: method, functionname: null};
-    })
-    .filter(mset => mset.functionname)
-    .forEach((mset) => {
-      // @ts-ignore
-      router[mset.method](`${mpath}${(basename === "index" ? "" : basename)}`.replace(/\\/g, "/"), tempm[mset.functionname]);
+    // .filter((file) => path.extname(file) === ".js")
+    .filter((file) => path.basename(file) === "index.js")
+    .forEach((file) => {
+      const f = path.relative(provided_path, file);
+      const tempm = require(file);
+      const mpath = `/${f.slice(0, f.lastIndexOf(path.sep) + 1)}`.replace(
+        /_/g,
+        ":"
+      );
+      const basename = path.basename(f, path.extname(f));
+      ["get", "post", "put", "delete"]
+        .map((method) => {
+          for (const tempmethod in tempm) {
+            if (tempmethod.toUpperCase() === method.toUpperCase()) {
+              return { method: method, functionname: tempmethod };
+            }
+          }
+          return { method: method, functionname: null };
+        })
+        .filter((mset) => mset.functionname)
+        .forEach((mset) => {
+          // @ts-ignore
+          router[mset.method](
+            `${mpath}${basename === "index" ? "" : basename}`.replace(
+              /\\/g,
+              "/"
+            ),
+            // @ts-ignore
+            tempm[mset.functionname]
+          );
+        });
     });
-  });
   return Promise.resolve(router);
-
-};
+}
 
 export default attachDirToRouter;
