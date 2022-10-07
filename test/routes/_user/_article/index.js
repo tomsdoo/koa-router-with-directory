71fb2fafaf526182959148da0c02b33d894abcf4
tom <toms@live.jp>
2022-10-08 08:59:52 +0900

    tests for replacing

---
+++ test/routes/_user/_article/index.js
@@ -0,0 +1,5 @@
+exports.get = function(ctx, next) {
+  ctx.response.status = 200;
+  ctx.response.type = "application/json";
+  ctx.response.body = JSON.stringify({message: "ok"});
+};
--- test/test.ts
+++ test/test.ts
@@ -14,13 +14,26 @@ describe(`routes`, () => {
     );
   });
 
-  it("count of paths", () => {
-    const pathSet = new Set(router.stack.map(({ path }) => path));
-    assert.equal(pathSet.size, 2);
+  it("paths", () => {
+    const expected = [
+      "/:user/:article/",
+      "/:user/:connection_string/",
+      "/path1/:name/",
+      "/path2/:value/",
+    ];
+
+    assert.equal(
+      JSON.stringify(
+        Array.from(new Set(router.stack.map(({ path }) => path))).sort()
+      ),
+      JSON.stringify(expected.sort())
+    );
   });
 
   it("combinations", () => {
     const expected = [
+      { method: "GET", path: "/:user/:article/" },
+      { method: "GET", path: "/:user/:connection_string/" },
       { method: "GET", path: "/path1/:name/" },
       { method: "GET", path: "/path2/:value/" },
       { method: "POST", path: "/path2/:value/" },
