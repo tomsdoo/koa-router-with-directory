8015578424a911a8f77b552506578de056468827
tom <toms@live.jp>
2022-10-07 14:05:56 +0900

    parserOptions.project is changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.base.json'
   },
   rules: {
   }
