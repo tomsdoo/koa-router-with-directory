0922ce5adb3d6fa4dbe2c3ae3eb9d73dd6baa757
tom <toms@live.jp>
2022-10-07 17:04:06 +0900

    lint:test fix

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import Router from "koa-router";
 import { attachDirToRouter } from "../src/index";
 import * as path from "path";
@@ -7,16 +6,16 @@ import { describe, it, before } from "mocha";
 
 const router = new Router();
 
-let r_router = router;
+let rRouter = router;
 
 describe(`routes`, () => {
   before(async () => {
-    r_router = await attachDirToRouter(router, path.join(__dirname, "routes/"));
+    rRouter = await attachDirToRouter(router, path.join(__dirname, "routes/"));
   });
 
   it("count of paths", () => {
     const pathSet = new Set();
-    r_router.stack.forEach(({ path }) => {
+    rRouter.stack.forEach(({ path }) => {
       pathSet.add(path);
     });
     assert.equal(pathSet.size, 2);
@@ -31,7 +30,7 @@ describe(`routes`, () => {
 
     assert(
       expected.every(({ method, path }) =>
-        r_router.stack.find(
+        rRouter.stack.find(
           ({ methods, path: layerPath }) =>
             methods.includes(method) && path === layerPath
         )
