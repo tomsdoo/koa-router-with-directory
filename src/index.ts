5c400a97c9eb0b178c896cc6d12203dbfc401c36
tom <toms@live.jp>
2024-01-08 10:52:38 +0900

    require() -> import()

--- src/index.ts
+++ src/index.ts
@@ -26,40 +26,36 @@ export async function attachDirToRouter(
     return files;
   })(providedPath);
 
-  files
-    // .filter((file) => path.extname(file) === ".js")
-    .filter((file) => path.basename(file) === "index.js")
-    .forEach((file) => {
-      const f = path.relative(providedPath, file);
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const tempm = require(file);
-      const mpath = `/${f.slice(0, f.lastIndexOf(path.sep) + 1)}`.replace(
-        /\/_/g,
-        "/:"
-      );
-      const basename = path.basename(f, path.extname(f));
-      ["get", "post", "put", "delete"]
-        .map((method) => {
-          for (const tempmethod in tempm) {
-            if (tempmethod.toUpperCase() === method.toUpperCase()) {
-              return { method, functionname: tempmethod };
-            }
+  for (const file of files.filter(
+    (file) => path.basename(file) === "index.js"
+  )) {
+    const f = path.relative(providedPath, file);
+    const tempm = await import(file);
+    const mpath = `/${f.slice(0, f.lastIndexOf(path.sep) + 1)}`.replace(
+      /\/_/g,
+      "/:"
+    );
+    const basename = path.basename(f, path.extname(f));
+    ["get", "post", "put", "delete"]
+      .map((method) => {
+        for (const tempmethod in tempm) {
+          if (tempmethod.toUpperCase() === method.toUpperCase()) {
+            return { method, functionname: tempmethod };
           }
-          return { method, functionname: null };
-        })
-        .filter((mset) => mset.functionname)
-        .forEach((mset) => {
+        }
+        return { method, functionname: null };
+      })
+      .filter((mset) => mset.functionname)
+      .forEach((mset) => {
+        // @ts-expect-error
+        router[mset.method](
+          `${mpath}${basename === "index" ? "" : basename}`.replace(/\\/g, "/"),
           // @ts-expect-error
-          router[mset.method](
-            `${mpath}${basename === "index" ? "" : basename}`.replace(
-              /\\/g,
-              "/"
-            ),
-            // @ts-expect-error
-            tempm[mset.functionname]
-          );
-        });
-    });
+          tempm[mset.functionname]
+        );
+      });
+  }
+
   return await Promise.resolve(router);
 }
 
