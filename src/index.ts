150bfd181ea2f3f595b2377ab7ba20be5719e16f
tomsdoo <toms@live.jp>
2025-09-14 13:21:29 +0900

    support methods

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,68 @@
 import fg from "fast-glob";
+import http from "http";
 import * as path from "path";
+
+const METHODS = http.METHODS.map((method) => method.toLowerCase());
+
+function hasMethod(
+  router: unknown,
+  method: string,
+): router is {
+  [method]: (
+    path: string,
+    handler: (ctx: unknown, next: unknown) => Promise<void>,
+  ) => void;
+} {
+  if (router == null || typeof router !== "object") {
+    return false;
+  }
+  return method in router;
+}
 
 export async function attachDirToRouter<T>(
   router: T,
   source: string,
 ): Promise<T> {
+  if (router == null || typeof router !== "object") {
+    throw new Error("router instance is required");
+  }
+
   const files = await fg(`${source}/**/*.(ts|js)`, { onlyFiles: true });
   for (const file of files
     .filter((file) => path.basename(file) === "index.js")
     .sort()) {
     const f = path.relative(source, file);
-    const tempm = await import(file);
-    const mpath = `/${f.slice(0, f.lastIndexOf(path.sep) + 1)}`.replace(
+    const targetModule = await import(file);
+    const modulePath = `/${f.slice(0, f.lastIndexOf(path.sep) + 1)}`.replace(
       /\/_/g,
       "/:",
     );
     const basename = path.basename(f, path.extname(f));
-    ["get", "post", "put", "delete"]
-      .map((method) => {
-        for (const tempmethod in tempm) {
-          if (tempmethod.toUpperCase() === method.toUpperCase()) {
-            return { method, functionname: tempmethod };
-          }
+    METHODS.map((method) => {
+      for (const targetMethod in targetModule) {
+        if (targetMethod.toUpperCase() === method.toUpperCase()) {
+          return { method, handlerName: targetMethod };
         }
-        return { method, functionname: null };
-      })
-      .filter(({ functionname }) => functionname != null)
-      .forEach(({ method, functionname }) => {
-        // @ts-expect-error method exists
-        router[method as Method](
-          `${mpath}${basename === "index" ? "" : basename}`.replace(/\\/g, "/"),
-          // @ts-expect-error functionname exists
-          tempm[functionname],
+      }
+      return { method, handlerName: null };
+    })
+      .filter(
+        (module): module is { method: string; handlerName: string } =>
+          module.handlerName != null,
+      )
+      .forEach(({ method, handlerName }) => {
+        if (method in router === false) {
+          return;
+        }
+        if (hasMethod(router, method) === false) {
+          return;
+        }
+        router[method](
+          `${modulePath}${basename === "index" ? "" : basename}`.replace(
+            /\\/g,
+            "/",
+          ),
+          targetModule[handlerName],
         );
       });
   }
